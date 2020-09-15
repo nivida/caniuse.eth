add028d694709ecac877005bfa62a90bc9243b6b
nivida <samuel@ethereum.org>
2020-09-15 12:27:52 +0200

    runner splitted up

--- runner/runner.go
+++ runner/runner.go
@@ -14,42 +14,43 @@ type Runner struct {
 	FailedCases []*worker.Job
 	SuccessfullCases []*worker.Job
 	results: chan<- interface{}
+	jobs: <-chan worker.Job
 }
 
 func New() (runner *Runner) {
 	return new(Runner)
 }
 
-// Starts the worker pipeline and will later use the Loader to get all test cases
 func (r *Runner) Start(config *provider.Config) {
-	// TODO: Implement buffered loader to push one after an other into the jobs pipeline (all at once takes a bit more memory on the same time)
-	/*
-	* loader := loader.New("testcases.json")
-	* taskChunk = <-loader.GetTasks()
-	*
-	*/
-	var tasks = make([]worker.Job, 4)
-	tasks[0] = worker.Job{Method: "eth_getBlockByNumber", Params: []interface{} {"latest", true}}
-	tasks[1] = worker.Job{Method: "eth_getBlockByNumber", Params: []interface{} {"latest", true}}
-	tasks[2] = worker.Job{Method: "eth_getBlockByNumber", Params: []interface{} {"latest", true}}
-	tasks[3] = worker.Job{Method: "eth_getBlockByNumber", Params: []interface{} {"latest", true}}
+	r.startWorkers(2)
+	r.passJobs()
+	r.processTestResults()
 
-	jobs := make(chan worker.Job, 2)
-	results := make(chan interface{}, 2)
+	// log.Println(approver.SuccessCount)
+	// log.Println(approver.FailureCount)
+	// log.Println(approver.FailedCases)
+	// log.Println(approver.SuccessfullCases)
+}
 
-	// Start 2 workers
-	for w := 1; w <= 2; w++ {
-		worker := worker.New(config, jobs, r.results)
-		go worker.Start()
-	}
+func (r *Runner) passJobs() {
+	tasks := r.getJobs() 
 
 	// Pass one task after another into the jobs channel for our workers
 	for _, v := range tasks {
-		jobs <- v
+		r.jobs <- v
 	}
 
 	close(jobs)
+}
 
+func (r *Runner) startWorkers(amount int) {
+	for w := 1; w <= amount; w++ {
+		worker := worker.New(config, jobs, r.results)
+		go worker.Start()
+	}
+}
+
+func (r *Runner) processTestResults() {
 	for i := 1; i <= len(tasks); i++ {
 		result := approver.check(<-r.results)
 		if result.Successfull == true {
@@ -62,9 +63,20 @@ func (r *Runner) Start(config *provider.Config) {
 		r.FailureCount++
 		append(r.FailedCases, result)
 	} 
+}
 
-	// log.Println(approver.SuccessCount)
-	// log.Println(approver.FailureCount)
-	// log.Println(approver.FailedCases)
-	// log.Println(approver.SuccessfullCases)
+// TODO: Implement buffered loader to push one after an other into the jobs pipeline (all at once takes a bit more memory on the same time)
+/*
+* loader := loader.New("testcases.json")
+* taskChunk = <-loader.GetTasks()
+*
+*/
+func (r *Runner) getJobs() *[]worker.Job {
+	var tasks = make([]worker.Job, 4)
+	tasks[0] = worker.Job{Method: "eth_getBlockByNumber", Params: []interface{} {"latest", true}}
+	tasks[1] = worker.Job{Method: "eth_getBlockByNumber", Params: []interface{} {"latest", true}}
+	tasks[2] = worker.Job{Method: "eth_getBlockByNumber", Params: []interface{} {"latest", true}}
+	tasks[3] = worker.Job{Method: "eth_getBlockByNumber", Params: []interface{} {"latest", true}}
+
+	return tasks
 }
